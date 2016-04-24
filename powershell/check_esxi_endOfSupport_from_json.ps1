22c79aa566913d6f221626eb811684fd3a763121
Florian Grehl <git@virten.net>
2016-04-24 13:15:17 +0200

    ESXi End of Support check script
    Script to check ESXi End of Support date with www.virten.net Support
    Lifecycle JSON file

---
+++ powershell/check_esxi_endOfSupport_from_json.ps1
@@ -0,0 +1,23 @@
+﻿$vmwareProductLifecycle = Invoke-WebRequest -Uri http://www.virten.net/repo/vmwareProductLifecycle.json | ConvertFrom-Json
+$vmHosts = Get-VMHost
+
+Foreach ($vmHost in $vmHosts) {
+  $releaseFound = $false
+  $product = "VMware ESXi $($vmHost.ApiVersion)"
+
+  Write-Host "$($vmHost.Name) is running Version $($product)"
+  
+  Foreach ($prod in $vmwareProductLifecycle.data.productLifecycle) {
+    If ($product -eq $prod.productRelease) {
+      $TimeSpanEogs = New-TimeSpan –Start (get-date) –End (get-date $prod.endOfGeneralSupport)
+      $TimeSpanEotg = New-TimeSpan –Start (get-date) –End (get-date $prod.endOfTechnicalGuidance)
+      Write-Host " - End of Genereal Support: $($prod.endOfGeneralSupport) ($($TimeSpanEogs.days) Days)"
+      Write-Host " - End of Technical Guidance: $($prod.endOfTechnicalGuidance) ($($TimeSpanEotg.days) Days)"
+      $releaseFound = $true
+    }
+  }
+  
+  If (-Not $releaseFound){
+    Write-Host " - Build $($vmHost.Build) not found in database!" -ForegroundColor Red
+  }
+}
