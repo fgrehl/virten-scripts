a8a3fcfc7a2eede4e0f98d769638c1091cfc453f
Florian Grehl <git@virten.net>
2016-12-26 18:03:07 +0100

    ESXi info to array for csv export

--- powershell/check_esxi_version_with_json.ps1
+++ powershell/check_esxi_version_with_json.ps1
@@ -5,16 +5,23 @@
 
 $esxiReleases = Invoke-WebRequest -Uri http://www.virten.net/repo/esxiReleases.json | ConvertFrom-Json
 $vmHosts = Get-VMHost
+$AllInfo = @()
 
 Foreach ($vmHost in $vmHosts) {
   $buildFound = $false
+  $Info = "" | Select VMHost, Build, ReleaseLevel, PatchLevel, ReleaseDate, UpdateAvailable, LatestPatch, LatestBuild
   Write-Host "ESXi Host $($vmHost.Name) is running on Build $($vmHost.Build)"
+  $Info.VMHost = $vmHost.Name
+  $Info.Build = $vmHost.Build
 
   Foreach ($release in $esxiReleases.data.esxiReleases) {
     If ($vmHost.Build -eq $release.Build) {
       Write-Host " - Release Level: $($release.releaseLevel)"
+      $Info.ReleaseLevel = $release.releaseLevel
       Write-Host " - Patch Level: $($release.friendlyName)"
+      $Info.PatchLevel = $release.friendlyName
       Write-Host " - Release Date: $($release.releaseDate)"
+      $Info.ReleaseDate = $release.releaseDate
       $minorRelease = $($release.minorRelease)
       $buildFound = $true
              
@@ -28,12 +35,20 @@ Foreach ($vmHost in $vmHosts) {
     
       if($latestBuild -eq $vmHost.Build) {
         Write-Host " - $($vmHost.Name) is running the latest version!"  -ForegroundColor Green
+        $Info.UpdateAvailable = $false
       } else {
         Write-Host " - $($vmHost.Name) update available: $($latestPatch) ($($latestBuild))"  -ForegroundColor Red
+        $Info.UpdateAvailable = $true
+        $Info.LatestPatch = $latestPatch
+        $Info.LatestBuild = $latestBuild
       }
     }
   }
   If (-Not $buildFound){
     Write-Host " - Build $($vmHost.Build) not found in database!" -ForegroundColor Red
   }
+  $AllInfo += $Info
 }
+
+
+# $AllInfo | Export-Csv -NoTypeInformation esxi.csv
