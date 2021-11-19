3a16f362b2ef047c3cb6a2c1f54e7cfc4505ce1d
Florian Grehl <florian.grehl@akquinet.de>
2021-11-19 10:27:06 +0100

    Get kubeconfig from VMware Cloud Director 10.3 with CSE

---
+++ bash/vcd_get_kubeconfig.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Get kubeconfig from VMware Cloud Director 10.3 with Container Service Extension
+# Usage: getconfig2.sh --user USER@ORG --password PASSWORD -vcd VCDURL --cluster CLUSTERNAME
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -u|--user) USER="$2"; shift ;;
+        -p|--password) PASSWORD="$2"; shift ;;
+        -v|--vcd) VCD="$2"; shift ;;
+        -c|--cluster) CLUSTER="$2"; shift ;;
+        *) echo "Unknown parameter: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+if [[ -z $CLUSTER || -z $VCD ]]; then
+ echo 'Missing parameter. Try:'
+ echo 'getconfig2.sh -u USER@ORG -p PASSWORD -v VCDURL -c CLUSTERNAME'
+ echo 'getconfig2.sh --user USER@ORG --password PASSWORD -vcd VCDURL --cluster CLUSTERNAME'
+ exit 1
+fi
+
+ACCEPT="Accept: */*;version=36.0"
+CRED=$(echo -n $USER:$PASSWORD |base64)
+TOKEN=$(curl -sI -k --header "$ACCEPT" --header "Authorization: Basic $CRED" --request POST https://$VCD/api/sessions | tr -d '\r' | sed -En 's/^x-vcloud-authorization: (.*)/\1/p')
+CLUSTERID=$(curl -s -k --header "$ACCEPT" --header "x-vcloud-authorization: $TOKEN" --request GET https://$VCD/api/cse/3.0/cluster | jq  '.[] | select(.name=="'$CLUSTER'")' | jq -r  '.id')
+curl -s -k --header "$ACCEPT" --header "x-vcloud-authorization: $TOKEN" --request GET https://$VCD/api/cse/3.0/cluster/$CLUSTERID/config | jq -r '.message' #  | tr -d '"' |sed 's/\\n/\n/g'
